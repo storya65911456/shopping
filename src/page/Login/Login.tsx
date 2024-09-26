f74e074280e42c3f38601bef5a27531c05c7327f
storya65911456 <storya65911456@gmail.com>
2024-09-26 18:00:56 +0800

    0926

--- src/page/Login/Login.tsx
+++ src/page/Login/Login.tsx
@@ -1,3 +1,8 @@
+import {
+    CredentialResponse,
+    GoogleLogin,
+    GoogleOAuthProvider
+} from '@react-oauth/google';
 import { useEffect, useRef, useState } from 'react';
 import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
 import { CiLock } from 'react-icons/ci';
@@ -17,6 +22,15 @@ const Login = () => {
     const accountRef = useRef<HTMLInputElement>(null);
     const passwordRef = useRef<HTMLInputElement>(null);
 
+    const handleLoginSuccess = (response: CredentialResponse) => {
+        console.log('Login successful, token:', response.credential);
+        // 你可以使用這個 token 與後端進行身份驗證
+    };
+
+    const handleLoginFailure = () => {
+        console.error('Login failed');
+    };
+
     const togglePasswordVisibility = () => {
         setPasswordVisible(!passwordVisible);
     };
@@ -28,79 +42,100 @@ const Login = () => {
     };
 
     return (
-        <div className='h-screen2 bg-Mwhite text-[#2c3338]'>
-            <div className='h-[10%] w-full bg-Mwhite shadow-sm'>
-                <DarkMode />
+        <GoogleOAuthProvider clientId='291747613276-mosebdke0p2d81kg9e3pgig8actjaitn.apps.googleusercontent.com'>
+            <div className='h-screen2 bg-Mwhite text-[#2c3338]'>
+                <div className='h-[10%] w-full bg-Mwhite shadow-sm'>
+                    <DarkMode />
+                </div>
+                <div className='h-[85%] w-full p-5'>
+                    <div className='h-[20%] w-full flex justify-center items-center'>
+                        <SiShopee fontSize={'3.5rem'} color={'shopee'} />
+                    </div>
+                    <div className='h-[2%]'></div>
+                    <div className='h-[45%] w-full'>
+                        {/* 帳號輸入 */}
+                        <div className='h-[25%] w-full flex justify-center items-center border-b border-[#2c3338]'>
+                            <SlUser fontSize={'1.2rem'} className='w-[10%]' />
+                            <input
+                                type='text'
+                                ref={accountRef}
+                                className='w-[90%] bg-transparent pl-2 focus:outline-none'
+                                placeholder='電話號碼/Email/使用者名稱'
+                                onInput={handleInputChange} // 追蹤輸入變動
+                            />
+                        </div>
+                        {/* 密碼輸入 */}
+                        <div className='h-[25%] w-full flex justify-center items-center border-b border-[#2c3338]'>
+                            <CiLock fontSize={'1.5rem'} className='w-[10%]' />
+                            <input
+                                type={passwordVisible ? 'text' : 'password'}
+                                ref={passwordRef}
+                                className='w-[60%] bg-transparent pl-2 focus:outline-none'
+                                placeholder='密碼'
+                                onInput={handleInputChange} // 追蹤輸入變動
+                            />
+                            <div
+                                onClick={togglePasswordVisibility}
+                                className='w-[15%] flex justify-center items-center border-r border-[#2c3338]'
+                            >
+                                {passwordVisible ? (
+                                    <AiFillEyeInvisible size={24} />
+                                ) : (
+                                    <AiFillEye size={24} />
+                                )}
+                            </div>
+                            <div className='w-[15%] flex justify-center items-center'>
+                                忘記?
+                            </div>
+                        </div>
+                        {/* 登入按鈕 */}
+                        <div className='h-[30%] py-3'>
+                            <button
+                                className={`h-full w-full text-white ${
+                                    isLoginEnabled ? 'bg-[#ee4d2d]' : 'bg-gray-400/40'
+                                }`}
+                                disabled={!isLoginEnabled} // 當帳號或密碼為空時禁用按鈕
+                            >
+                                登入
+                            </button>
+                        </div>
+                        {/* 註冊 */}
+                        <div className='h-[20%]'>
+                            <div className='h-full w-full flex items-center justify-between'>
+                                <div className='h-full w-[30%]'>註冊</div>
+                                <div className='h-full w-[30%]'>使用簡訊登入</div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* 三方登入 */}
+                    <div className='h-[33%]'>
+                        <div className='flex items-center justify-center h-[20%]'>
+                            <div className='w-20 border-t border-gray-400'></div>
+                            <span className='mx-2 text-gray-500'>或</span>
+                            <div className='w-20 border-t border-gray-400'></div>
+                        </div>
+                        <div className='h-[75%] pt-2'>
+                            <GoogleLogin
+                                onSuccess={handleLoginSuccess}
+                                onError={handleLoginFailure}
+                                text='signin_with'
+                            />
+                            <GoogleLogin
+                                onSuccess={handleLoginSuccess}
+                                onError={handleLoginFailure}
+                                text='signin_with'
+                            />
+                            <GoogleLogin
+                                onSuccess={handleLoginSuccess}
+                                onError={handleLoginFailure}
+                                text='signin_with'
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div className='h-[5%]'>4</div>
             </div>
-            <div className='h-[85%] w-full p-5'>
-                <div className='h-[20%] w-full flex justify-center items-center'>
-                    <SiShopee fontSize={'3.5rem'} color={'shopee'} />
-                </div>
-                <div className='h-[2%]'></div>
-                <div className='h-[50%] w-full'>
-                    {/* 帳號輸入 */}
-                    <div className='h-[20%] w-full flex justify-center items-center border-b border-[#2c3338]'>
-                        <SlUser fontSize={'1.2rem'} className='w-[10%]' />
-                        <input
-                            type='text'
-                            ref={accountRef}
-                            className='w-[90%] bg-transparent pl-2 focus:outline-none'
-                            placeholder='電話號碼/Email/使用者名稱'
-                            onInput={handleInputChange} // 追蹤輸入變動
-                        />
-                    </div>
-                    {/* 密碼輸入 */}
-                    <div className='h-[20%] w-full flex justify-center items-center border-b border-[#2c3338]'>
-                        <CiLock fontSize={'1.5rem'} className='w-[10%]' />
-                        <input
-                            type={passwordVisible ? 'text' : 'password'}
-                            ref={passwordRef}
-                            className='w-[60%] bg-transparent pl-2 focus:outline-none'
-                            placeholder='密碼'
-                            onInput={handleInputChange} // 追蹤輸入變動
-                        />
-                        <div
-                            onClick={togglePasswordVisibility}
-                            className='w-[15%] flex justify-center items-center border-r border-[#2c3338]'
-                        >
-                            {passwordVisible ? (
-                                <AiFillEyeInvisible size={24} />
-                            ) : (
-                                <AiFillEye size={24} />
-                            )}
-                        </div>
-                        <div className='w-[15%] flex justify-center items-center'>
-                            忘記?
-                        </div>
-                    </div>
-                    {/* 登入按鈕 */}
-                    <div className='h-[30%] py-3'>
-                        <button
-                            className={`h-full w-full text-white ${
-                                isLoginEnabled ? 'bg-[#ee4d2d]' : 'bg-gray-400/40'
-                            }`}
-                            disabled={!isLoginEnabled} // 當帳號或密碼為空時禁用按鈕
-                        >
-                            登入
-                        </button>
-                    </div>
-                    <div className='h-[20%]'>
-                        <div className='h-full w-full flex items-center justify-between'>
-                            <div className='h-full w-[30%]'>註冊</div>
-                            <div className='h-full w-[30%]'>使用簡訊登入</div>
-                        </div>
-                    </div>
-                </div>
-                <div className='h-[28%]'>
-                    <div className='flex items-center justify-center'>
-                        <div className='w-20 border-t border-gray-400'></div>
-                        <span className='mx-2 text-gray-500'>或</span>
-                        <div className='w-20 border-t border-gray-400'></div>
-                    </div>
-                </div>
-            </div>
-            <div className='h-[5%]'>4</div>
-        </div>
+        </GoogleOAuthProvider>
     );
 };
 
